0f857556fb119d2a0132d0f405b154866ebba90a
Chris O'Neil <cconeil5@gmail.com>
2015-11-05 22:26:43 -0800

    cleaned up extra pods

--- STP Example/Pods/Target Support Files/STP/STP-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_STP : NSObject
-@end
-@implementation PodsDummy_STP
-@end
--- STP Example/Pods/Target Support Files/STP/STP-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "STP.h"
-
-FOUNDATION_EXPORT double STPVersionNumber;
-FOUNDATION_EXPORT const unsigned char STPVersionString[];
-
